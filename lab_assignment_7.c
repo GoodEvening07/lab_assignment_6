f9d661c11adc67113c16e6af334baf246bceb17a
GoodEvening07 <72945997+GoodEvening07@users.noreply.github.com>
2023-07-13 18:34:50 -0400

    Add files via upload
    Lab 7 Assignment last file got corrupt for some odd reason as I was trying to test it beforehand. Please let me know if there are any issues and I will notify the professor of this issue as well :)

---
+++ lab_assignment_7.c
@@ -0,0 +1,59 @@
+/*Gabriel Hernandez
+Lab 7 - There was an error with my github that the lab 7 file got corrupt hence this is a replacement.
+*/
+
+#include <stdio.h>
+
+void swap(int *xP, int *yP);
+void bubbleSort(int arr[], int n);
+void printArray(int arr[], int size);
+int main()
+{
+  int n = 9;
+  int item[] = {97, 16, 45, 63, 13, 22, 7, 58, 72};
+
+  printf("Array bofore sorting: \n");
+  printArray(item, n);
+  bubbleSort(item, n);
+  printf("\nSorted array: \n");
+  printArray(item, n);
+  return 0;
+}
+
+void swap(int *xP, int *yP)
+{
+  int temp = *xP;
+  *xP = *yP;
+  *yP = temp;
+}
+
+void bubbleSort(int arr[], int n)
+{
+  int a, b, temp;
+  for (a = 0; a < n - 1; a++)
+  {
+  printf("\nIteration# %d\n\n", a+1);
+    
+    for (b = 0; b < n-a-1; b++)
+    {
+      if (arr[b] > arr[b+1])
+      {
+        temp = arr[b];
+        arr[b] = arr[b+1];
+        arr[b+1] = temp;
+      }
+     printArray(arr, n);
+    }
+  }
+}
+
+/* Function to print an array */
+void printArray(int arr[], int size)
+{
+  int i;
+  for (i=0; i < size; i++)
+  {
+    printf("%d ", arr[i]);
+  }
+  printf("\n");
+}
